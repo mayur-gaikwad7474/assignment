ea4c1cc695074548cd40949cf022103875a8b1a7
Mayur Gaikwad <mayur.gaikward@aitrade.ai>
2021-01-01 22:40:53 +0530

    added files and code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [content, setcontent] = useState("")
+  const [tags, settags] = useState("")
+  const [slug, setslug] = useState("")
+  const [file, setfile] = useState("")
+  const [id, setid] = useState(null)
+  const [arrayOfPost, setarrayOfPost] = useState([])
+
+  const getFile = (e) => {
+    setfile(e.target.files[0])
+  }
+
+  const addHashTag = (e) => {
+    settags(e.target.value)
+  }
+
+  const uploadPost = () => {
+    if (content === "" || tags === "" || slug === "" || file === "") return alert("plz fill all fields")
+    const data = {
+      id: Date.now(),
+      content: content,
+      resource: {
+        url: slug,
+        tags: tags,
+        fileData: file,
+        file: "http://image.com/" + file.name,
+      }
+    }
+    setarrayOfPost((prev) => [...prev, data])
+  }
+
+
+  const editPost = (data) => {
+    setcontent(data.content)
+    setslug(data.resource.url)
+    settags(data.resource.tags)
+    setid(data.id)
+    setfile(data.resource.fileData)
+    document.getElementById("file").value = ""
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="editor-container">
+        <div className="input-container">
+          <label>Content</label>
+          <input value={content} type="text" placeholder="your content"
+            onChange={(e) => setcontent(e.target.value)} />
+        </div>
+        <div className="input-container">
+          <label>Add URL</label>
+          <input value={slug} type="text" placeholder="url"
+            onChange={(e) => setslug(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Tag</label>
+          <input value={tags} type="text" className="tags" placeholder="#tags"
+            onChange={(e) => addHashTag(e)}
+          />
+        </div>
+        <div>
+          <input id="file" type="file" onChange={(e) => getFile(e)} />
+        </div>
+        <button onClick={uploadPost}>Upload</button>
+      </div>
+      <div className="posts">
+        {
+          arrayOfPost.map(data => {
+            return <div key={data.id} className="card" onClick={() => editPost(data)}>
+              <div className="edit">✎</div>
+              <div>Content : {data.content}</div>
+              <div>URL : {data.resource.url}</div>
+              <div>Tags : <span style={{ color: 'blue' }} >{data.resource.tags}</span></div>
+              <div>File URL : {data.resource.file} </div>
+            </div>
+          })
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
