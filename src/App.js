dd010f76d1f053e6bc4ef8c0999b4211fd9ce8fc
Mayur Gaikwad <mayur.gaikward@aitrade.ai>
2021-01-02 08:15:14 +0530

    added edit and post preview

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const App = () => {
   const [tags, settags] = useState("")
   const [slug, setslug] = useState("")
   const [file, setfile] = useState("")
+  const [fileURL, setfileURL] = useState("")
   const [id, setid] = useState(null)
   const [arrayOfPost, setarrayOfPost] = useState([])
 
   const getFile = (e) => {
     setfile(e.target.files[0])
+    const reader = new FileReader();
+    reader.onload = function () {
+      setfileURL(reader.result)
+    };
+    reader.readAsDataURL(e.target.files[0]);
   }
 
   const addHashTag = (e) => {
@@ -20,17 +26,46 @@ const App = () => {
 
   const uploadPost = () => {
     if (content === "" || tags === "" || slug === "" || file === "") return alert("plz fill all fields")
-    const data = {
-      id: Date.now(),
-      content: content,
-      resource: {
-        url: slug,
-        tags: tags,
-        fileData: file,
-        file: "http://image.com/" + file.name,
+    let type = file.type
+    type = type.substring(0, 5)
+    console.log(type)
+    if (id !== null) {
+      const updatedArray = arrayOfPost.map(data => {
+        if (data.id === id) {
+          data.content = content
+          data.resource.url = slug
+          data.resource.tags = tags
+          data.resource.fileData = file
+          data.resource.type = type
+          data.resource.file = "http://image.com/" + file.name
+          data.resource.fileURL = fileURL
+          return data
+        } else {
+          return data
+        }
+      })
+      setid(null)
+      setarrayOfPost(updatedArray)
+    } else {
+      const data = {
+        id: Date.now(),
+        content: content,
+        resource: {
+          url: slug,
+          tags: tags,
+          fileData: file,
+          type: type,
+          file: "http://image.com/" + file.name,
+          fileURL: fileURL
+        }
       }
+      setarrayOfPost((prev) => [...prev, data])
     }
-    setarrayOfPost((prev) => [...prev, data])
+    setcontent("")
+    setslug("")
+    settags("")
+    setfile("")
+    document.getElementById("file").value = ""
   }
 
 
@@ -40,7 +75,6 @@ const App = () => {
     settags(data.resource.tags)
     setid(data.id)
     setfile(data.resource.fileData)
-    document.getElementById("file").value = ""
   }
 
   return (
@@ -64,7 +98,7 @@ const App = () => {
           />
         </div>
         <div>
-          <input id="file" type="file" onChange={(e) => getFile(e)} />
+          <input id="file" type="file" accept="image/*, video/*" onChange={(e) => getFile(e)} />
         </div>
         <button onClick={uploadPost}>Upload</button>
       </div>
@@ -77,6 +111,12 @@ const App = () => {
               <div>URL : {data.resource.url}</div>
               <div>Tags : <span style={{ color: 'blue' }} >{data.resource.tags}</span></div>
               <div>File URL : {data.resource.file} </div>
+              {
+                data.resource.type === "image" ? <img src={data.resource.fileURL} alt="" /> :
+                  <video width="150" height="150" controls>
+                    <source src={data.resource.fileURL} type="video/mp4" />
+                  </video>
+              }
             </div>
           })
         }
